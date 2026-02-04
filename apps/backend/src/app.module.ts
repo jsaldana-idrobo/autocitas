9b92d56af869bf75a2faf9c9608a2097b785e633
Sebastian <jsaldana.idrobo@gmail.com>
2026-02-04 10:52:20 -0500

    feat(backend): add health endpoints

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { AdminModule } from "./admin/admin.module";
 import { PublicModule } from "./public/public.module";
+import { HealthController } from "./health.controller";
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGODB_URI || "mongodb://localhost:27017/autocitas"),
     AdminModule,
     PublicModule
-  ]
+  ],
+  controllers: [HealthController]
 })
 export class AppModule {}
